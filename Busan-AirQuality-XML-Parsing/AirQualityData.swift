1499a93e8fadd2b0304ded9914d15a51f5112635
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-10-15 15:18:49 +0900

    Initial Commit

---
+++ Busan-AirQuality-XML-Parsing/AirQualityData.swift
@@ -0,0 +1,18 @@
+//
+//  AirQualityData.swift
+//  Busan-AirQuality-XML-Parsing
+//
+//  Created by D7702_09 on 2018. 10. 15..
+//  Copyright © 2018년 jhi. All rights reserved.
+//
+
+import Foundation
+
+class AirQuailtyData {
+    var dPm10 = ""
+    var dPm25 = ""
+    var dPm10Cai = ""
+    var dPm25Cai = ""
+    var dSite = ""
+
+}
